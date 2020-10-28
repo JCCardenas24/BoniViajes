a4ba419aab89f46655638a3962e031b97b211fb6
Juan Carlos Cárdenas <juancarlos.cardenas@aesir.com.mx>
2020-10-28 12:57:33 -0600

    Estructura del sitio

---
+++ index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>The Travel - Tour Travel</title>
+    <?php include_once('./template/head.php') ?>
+</head>
+<body>
+    <div id="preloader">
+        <div id="status">&nbsp;</div>
+    </div>
+    <?php include_once('./template/header.php') ?>
+    <section>
+		<div>
+			<div>
+				<div>
+			        <div class="slider fullscreen" data-interval="false">
+                        <ul class="slides">
+                            <li>
+                                <img src="./assets/images/slider/1.jpg" alt="">
+                            </li>
+                            <li>
+                                <img src="./assets/images/slider/2.jpg" alt="">
+                            </li>
+                            <li>
+                                <img src="./assets/images/slider/3.jpg" alt="">
+                            </li>
+                            <li>
+                                <img src="./assets/images/slider/4.jpg" alt="">
+                            </li>
+                            <div class="col-md-6">
+                                <form class="row pos-form col-md-11">
+                                    <div class="col-md-12">
+                                        <input type="text" id="nombre" class="style-inp" placeholder="Nombre">
+                                    </div>
+                                    <div class="col-md-6 mt-15">
+                                        <input type="mail" id="mail" class="style-inp" placeholder="Correo electrónico">
+                                    </div>
+                                    <div class="col-md-6 mt-15">
+                                        <input type="text" id="phone" class="style-inp" placeholder="Teléfono">
+                                    </div>
+                                    <div class="col-md-12 mt-15">
+                                        <input type="text" id="viaje" class="style-inp" placeholder="¿A dónde viajas?">
+                                    </div>
+                                    <div class="col-md-12 mt-15">
+                                        <button class="btn col-md-12 materialize-red white-text">¡Quiero que me llamen!</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </ul>
+                    </div>
+				</div>
+			</div>
+		</div>
+	</section>
+    <section>
+        <div class="rows">
+            <div class="container">
+                <div class="text-center p-15">
+                    <h1 class="text-uppercase">life <span class="c-yellow h1 fw-b">style</span></h1>
+                    <div class="title-line">
+                        <div class="tl-1"></div>
+                    </div>
+                    <p class="c-gray h3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
+                <div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="v_place_img">
+                            <img src="./assets/images/t5.png" alt="Tour Booking" title="Tour Booking">
+                        </div>
+                        <div class="b_pack rows">
+                            <div class="col-md-12 col-sm-12">
+                                <h4><a href="tour-details.html">Nobul Hotel <br> los cabos</a></h4>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <p class="m-0 c-red fw-b">Noche desde</p>
+                                <p class="m-0 c-red h2 fw-b">$1,500</p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 h-70 pos-btn">
+                                <button class="btn col-md-12 btn-yellow white-text mb-10 radius-0 text-uppercase fw-b">Llamar</button>
+                                <button class="btn col-md-12 btn-gray white-text radius-0 fw-b">+ Información</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="v_place_img">
+                            <img src="./assets/images/t5.png" alt="Tour Booking" title="Tour Booking">
+                        </div>
+                        <div class="b_pack rows">
+                            <div class="col-md-12 col-sm-12">
+                                <h4><a href="tour-details.html">Nobul Hotel <br> los cabos</a></h4>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <p class="m-0 c-red ">Noches desde</p>
+                                <p class="m-0 c-red  h2">$1,500</p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 h-70 pos-btn">
+                                <button class="btn col-md-12 btn-yellow white-text mb-10 radius-0 text-uppercase fw-b">Llamar</button>
+                                <button class="btn col-md-12 btn-gray white-text radius-0 fw-b">+ Información</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="v_place_img">
+                            <img src="./assets/images/t5.png" alt="Tour Booking" title="Tour Booking">
+                        </div>
+                        <div class="b_pack rows">
+                            <div class="col-md-12 col-sm-12">
+                                <h4><a href="tour-details.html">Nobul Hotel <br> los cabos</a></h4>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <p class="m-0 c-red ">Noches desde</p>
+                                <p class="m-0 c-red h2">$1,500</p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 h-70 pos-btn">
+                                <button class="btn col-md-12 btn-yellow white-text mb-10 radius-0 text-uppercase fw-b">Llamar</button>
+                                <button class="btn col-md-12 btn-gray white-text radius-0 fw-b">+ Información</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="v_place_img">
+                            <img src="./assets/images/t5.png" alt="Tour Booking" title="Tour Booking">
+                        </div>
+                        <div class="b_pack rows">
+                            <div class="col-md-12 col-sm-12">
+                                <h4><a href="tour-details.html">Nobul Hotel <br> los cabos</a></h4>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <p class="m-0 c-red ">Noches desde</p>
+                                <p class="m-0 c-red  h2">$1,500</p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 h-70 pos-btn">
+                                <button class="btn col-md-12 btn-yellow white-text mb-10 radius-0 text-uppercase fw-b">Llamar</button>
+                                <button class="btn col-md-12 btn-gray white-text radius-0 fw-b">+ Información</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="v_place_img">
+                            <img src="./assets/images/t5.png" alt="Tour Booking" title="Tour Booking">
+                        </div>
+                        <div class="b_pack rows">
+                            <div class="col-md-12 col-sm-12">
+                                <h4><a href="tour-details.html">Nobul Hotel <br> los cabos</a></h4>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <p class="m-0 c-red ">Noches desde</p>
+                                <p class="m-0 c-red  h2">$1,500</p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 h-70 pos-btn">
+                                <button class="btn col-md-12 btn-yellow white-text mb-10 radius-0 text-uppercase fw-b">Llamar</button>
+                                <button class="btn col-md-12 btn-gray white-text radius-0 fw-b">+ Información</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="rows">
+            <div class="container">
+                <div class="text-center p-15">
+                    <h1 class="text-uppercase">Trendy <span class="c-yellow h1 fw-b">destinations</span></h1>
+                    <div class="title-line">
+                        <div class="tl-1"></div>
+                    </div>
+                    <p class="c-gray h3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
+                <div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="v_place_img">
+                            <img src="./assets/images/t5.png" alt="Tour Booking" title="Tour Booking">
+                        </div>
+                        <div class="b_pack rows">
+                            <div class="col-md-12 col-sm-12">
+                                <h4><a href="tour-details.html">Nobul Hotel <br> los cabos</a></h4>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <p class="m-0 c-red ">Noches desde</p>
+                                <p class="m-0 c-red  h2">$1,500</p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 h-70 pos-btn">
+                                <button class="btn col-md-12 btn-yellow white-text mb-10 radius-0 text-uppercase fw-b">Llamar</button>
+                                <button class="btn col-md-12 btn-gray white-text radius-0 fw-b">+ Información</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="v_place_img">
+                            <img src="./assets/images/t5.png" alt="Tour Booking" title="Tour Booking">
+                        </div>
+                        <div class="b_pack rows">
+                            <div class="col-md-12 col-sm-12">
+                                <h4><a href="tour-details.html">Nobul Hotel <br> los cabos</a></h4>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <p class="m-0 c-red ">Noches desde</p>
+                                <p class="m-0 c-red  h2">$1,500</p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 h-70 pos-btn">
+                                <button class="btn col-md-12 btn-yellow white-text mb-10 radius-0 text-uppercase fw-b">Llamar</button>
+                                <button class="btn col-md-12 btn-gray white-text radius-0 fw-b">+ Información</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="v_place_img">
+                            <img src="./assets/images/t5.png" alt="Tour Booking" title="Tour Booking">
+                        </div>
+                        <div class="b_pack rows">
+                            <div class="col-md-12 col-sm-12">
+                                <h4><a href="tour-details.html">Nobul Hotel <br> los cabos</a></h4>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <p class="m-0 c-red ">Noches desde</p>
+                                <p class="m-0 c-red  h2">$1,500</p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 h-70 pos-btn">
+                                <button class="btn col-md-12 btn-yellow white-text mb-10 radius-0 text-uppercase fw-b">Llamar</button>
+                                <button class="btn col-md-12 btn-gray white-text radius-0 fw-b">+ Información</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="rows">
+            <div class="container">
+                <div class="text-center p-15">
+                    <h1 class="text-uppercase">Top <span class="c-yellow h1 fw-b">of the month</span></h1>
+                    <div class="title-line">
+                        <div class="tl-1"></div>
+                    </div>
+                    <p class="c-gray h3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
+                <div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="v_place_img">
+                            <img src="./assets/images/t5.png" alt="Tour Booking" title="Tour Booking">
+                        </div>
+                        <div class="b_pack rows">
+                            <div class="col-md-12 col-sm-12">
+                                <h4><a href="tour-details.html">Nobul Hotel <br> los cabos</a></h4>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <p class="m-0 c-red ">Noches desde</p>
+                                <p class="m-0 c-red  h2">$1,500</p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 h-70 pos-btn">
+                                <button class="btn col-md-12 btn-yellow white-text mb-10 radius-0 text-uppercase fw-b">Llamar</button>
+                                <button class="btn col-md-12 btn-gray white-text radius-0 fw-b">+ Información</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="v_place_img">
+                            <img src="./assets/images/t5.png" alt="Tour Booking" title="Tour Booking">
+                        </div>
+                        <div class="b_pack rows">
+                            <div class="col-md-12 col-sm-12">
+                                <h4><a href="tour-details.html">Nobul Hotel <br> los cabos</a></h4>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <p class="m-0 c-red ">Noches desde</p>
+                                <p class="m-0 c-red  h2">$1,500</p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 h-70 pos-btn">
+                                <button class="btn col-md-12 btn-yellow white-text mb-10 radius-0 text-uppercase fw-b">Llamar</button>
+                                <button class="btn col-md-12 btn-gray white-text radius-0 fw-b">+ Información</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="v_place_img">
+                            <img src="./assets/images/t5.png" alt="Tour Booking" title="Tour Booking">
+                        </div>
+                        <div class="b_pack rows">
+                            <div class="col-md-12 col-sm-12">
+                                <h4><a href="tour-details.html">Nobul Hotel <br> los cabos</a></h4>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <p class="m-0 c-red ">Noches desde</p>
+                                <p class="m-0 c-red  h2">$1,500</p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 h-70 pos-btn">
+                                <button class="btn col-md-12 btn-yellow white-text mb-10 radius-0 text-uppercase fw-b">Llamar</button>
+                                <button class="btn col-md-12 btn-gray white-text radius-0 fw-b">+ Información</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include_once('./template/footer.php') ?>
+    <script src="./assets/js/jquery-latest.min.js"></script>
+    <script src="./assets/js/bootstrap.js"></script>
+    <script src="./assets/js/wow.min.js"></script>
+    <script src="./assets/js/materialize.min.js"></script>
+    <script src="./assets/js/custom.js"></script>
+</body>
+</html>
---
+++ template/head.php
@@ -0,0 +1,11 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="shortcut icon" href="images/fav.ico">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="./assets/css/font-awesome.min.css">
+<link rel="stylesheet" href="./assets/css/style.css">
+<link rel="stylesheet" href="./assets/css/materialize.css">
+<link rel="stylesheet" href="./assets/css/bootstrap.css">
+<link rel="stylesheet" href="./assets/css/mob.css">
+<link rel="stylesheet" href="./assets/css/animate.css">
+<link rel="stylesheet" href="./assets/css/styles-page.css">
